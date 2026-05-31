6004642f6612b6e22d6d2bb6f27a62e788667829
Wendel Batista <wendelbatista769@gmail.com>
2026-05-31 17:36:19 -0300

    Commit 1

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function app(){
+  return (
+    <div className= "min-h-screen bg-slate-900 flex items-center justify-center">
+      <h1 className= "text-5xl font-bold text-green-400">
+        REcraft
+      </h1>
+    </div>
+  )
+}
+
+export default app;
